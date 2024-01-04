c2b0b536fe6bee9dd891ef069e804546ac5dcae9
Parsa_Shabanpour <parsash1380@gmail.com>
2024-01-04 21:33:18 +0330

    add advertisement route

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,16 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const error = require("../middleware/error");
 const users = require("../controller/users");
+const advertisement = require("../controller/advertisement");
 const auth = require("../controller/auth");
 const cookieParser = require("cookie-parser");
 
 module.exports = function (app) {
-    app.use(express.json());
-    app.use(cors({credentials: true, origin: "http://localhost:3000"}));
-    app.use(cookieParser());
+  app.use(express.json());
+  app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+  app.use(cookieParser());
 
-    app.use("/api/auth", auth);
-    app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use("/api/users", users);
+  app.use("/api/advertisements", advertisement);
 
-    app.use(error);
+  app.use(error);
 };
